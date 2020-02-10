11c2f4f1a388d3bb28de7690d2cc30bd17febf18
Karen Daniela <kdaniimora@gmail.com>
2020-02-10 19:07:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+# code here
+
+school = School.new("Bayside High School")
+
+class initialize(name)
+  @name = name 
+school.add_student("Zach Morris", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+school.roster
+end 
+
+def school.grade(9)
+end 
+
+end 
